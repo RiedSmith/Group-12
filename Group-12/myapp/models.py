8a1dddc2df2cca3951377dcc422e1953d2aae4fc
Phillip Dominguez <Padom02@gmail.com>
2023-02-13 21:18:27 -0600

    Added superuser, 2 database tables, and integration with mySQLserver

--- Group-12/myapp/models.py
+++ Group-12/myapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# Models are the way we interact with the database
+class Listing(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField('date of listing')
+    price = models.PositiveIntegerField()
+    location = models.CharField(max_length=200)
+
+class User(models.Model):
+    username = models.CharField(max_length=15)
+    password = models.CharField(max_length=20)
+    ACCOUNT_TYPES = {
+        ('B', 'Buyer'),
+        ('S', 'Seller'),
+        ('A', 'Admin')
+    }
+    account_type = models.CharField(max_length=7, choices=ACCOUNT_TYPES)
