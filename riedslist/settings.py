26bd7ef086173d83d29dca6063c67afc86922b4a
RiedSmith <123522335+RiedSmith@users.noreply.github.com>
2023-04-16 19:25:03 -0500

    Update settings.py

--- riedslist/settings.py
+++ riedslist/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'PASSWORD': 'test_pass',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
         'TEST': {
             'USER': 'test_user',
             'PASSWORD': 'test_pass',
