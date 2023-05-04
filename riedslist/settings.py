18bb8c855c008bd5293baa0986651ec4469f596c
Phillip <Padom02@gmail.com>
2023-05-03 21:06:14 -0500

    Testing actions

--- riedslist/settings.py
+++ riedslist/settings.py
@@ -82,19 +82,32 @@ WSGI_APPLICATION = 'riedslist.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
-
+if os.getenv('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'workflow':{
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'github-actions',
+            'USER': 'sql',
+            'PASSWORD': 'sql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
     }
-}
+    }
+
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASS'),
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
+
+        }
+    }
 
 
 # Password validation
